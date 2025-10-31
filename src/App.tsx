92aea367ac9187f54886c3f03c4d8b8923b78e23
ValentinD <deyama2012@gmail.com>
2025-11-01 04:05:53 +0500

    Import fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from 'react';
-import { SelectedPage } from './shared/types';
-import Navbar from './scenes/navbar';
-import Home from './scenes/home';
-import Benefits from './scenes/benefits';
-import OurClasses from './scenes/ourClasses';
-import ContactUs from './scenes/contactUs';
-import Footer from './scenes/footer/footer';
+import { SelectedPage } from '@/shared/types';
+import Navbar from '@/scenes/navbar';
+import Home from '@/scenes/home';
+import Benefits from '@/scenes/benefits';
+import OurClasses from '@/scenes/ourClasses';
+import ContactUs from '@/scenes/contactUs';
+import Footer from '@/scenes/footer/footer';
 
 function App() {
   // Array deconstruction
