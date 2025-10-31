d33f8f958167b3888088c1f2e2a51437216370dc
ValentinD <deyama2012@gmail.com>
2025-11-01 03:16:32 +0500

    Footer

---
+++ src/scenes/footer/footer.tsx
@@ -0,0 +1,33 @@
+import Logo from '@/assets/Logo.png';
+
+const Footer = () => {
+  return (
+    <footer className="bg-primary-100 py-16">
+      <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
+        <div className="mt-16 basis-1/2 md:mt-0">
+          <img alt="logo" src={Logo} />
+          <p className="my-5">
+            Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum.
+          </p>
+          <p>© Evogym All Rights Reserved.</p>
+        </div>
+        <div className="mt-16 basis-1/4 md:mt-0">
+          <h4 className="font-bold">Links</h4>
+          <p className="my-5">Massa orci senectus</p>
+          <p className="my-5">Et gravida et etiam</p>
+          <p>Ullamcorper vivamus</p>
+        </div>
+        <div className="mt-16 basis-1/4 md:mt-0">
+          <h4 className="font-bold">Contact Us</h4>
+          <p className="my-5">Nemo enim ipsam voluptatem</p>
+          <p>(333) 425-6825</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
