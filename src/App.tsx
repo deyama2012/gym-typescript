7e3ca16aeb92e14b161b5c955c4a1dea0e6fbf7b
ValentinD <deyama2012@gmail.com>
2025-10-31 23:37:36 +0500

    Benefits page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import Navbar from './scenes/navbar';
 import { SelectedPage } from './shared/types';
 import Home from './scenes/Home';
+import Benefits from './scenes/Benefits';
 
 function App() {
   // Array deconstruction
@@ -30,6 +31,7 @@ function App() {
         setSelectedPage={setSelectedPage}
       />
       <Home setSelectedPage={setSelectedPage} />
+      <Benefits setSelectedPage={setSelectedPage} />
     </div>
   );
 }
--- src/shared/types.ts
+++ src/shared/types.ts
@@ -1,6 +1,14 @@
+import type { JSX } from 'react';
+
 export const SelectedPage = {
   Home: 'home',
   Benefits: 'benefits',
   OurClasses: 'ourclasses',
   ContactUs: 'contactus',
 };
+
+export interface BenefitType {
+  icon: JSX.Element;
+  title: string;
+  description: string;
+}
